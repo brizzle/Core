613b2c14705dc0eb68fb8456f1755332f7e4c291
Brock Billings <brock_billings@me.com>
2015-07-03 01:20:09 -0500

    Added DatabaseConfigService and LogConfigService

---
+++ Core.Services/DatabaseConfigService.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace Core.Services
+{
+    public class DatabaseConfigService
+    {
+        public DatabaseConfigService(string file, string path)
+        {
+            FileName = file;
+            FilePath = path;
+        }
+
+        public string ConnectionString
+        {
+            get
+            {
+                return Path.Combine("Data Source=" + FilePath, FileName + ".sqlie");
+            }
+        }
+
+        public string FileName { get; private set; }
+
+        public string FilePath { get; private set; }
+    }
+}
---
+++ Core.Services/LogConfigService.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Services
+{
+    public class LogConfigService
+    {
+        public LogConfigService(string file, string path)
+        {
+            FileName = file;
+            FilePath = path;
+        }
+
+        public string FileName { get; private set; }
+
+        public string FilePath { get; private set; }
+    }
+}
